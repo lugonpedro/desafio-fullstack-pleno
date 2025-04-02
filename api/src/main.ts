c9a3bb0996131f2819a9cddf9dc306567ed2ebe5
Pedro Lugon <pedromotta20@gmail.com>
2025-04-02 18:06:42 -0300

    feat: add auto validation, exception filter and swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,45 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './@shared/prisma/prisma_exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // CORS
+  app.enableCors();
+
+  // AUTO VALIDATION
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  // EXCEPTION FILTER DECORATOR
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
+
+  // API
+  const config = new DocumentBuilder()
+    .setTitle('Classes API')
+    .setDescription('description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  // SIGINT e SIGTERM shutdowns
+  app.enableShutdownHooks();
+
+  await app.listen(process.env.PORT);
+
+  const url = await app.getUrl();
+  if (url.split(':')[0] === 'http') {
+    console.log(`Application is running on: ${url}`);
+    console.log(`Swagger is running on: ${url}/docs`);
+  }
 }
 bootstrap();
